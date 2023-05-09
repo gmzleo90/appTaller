59f4139be21ba8f3c5630a89146eec26fa8ace0b
gmzleo90 <lagomezo@ext.falabella.cl>
2023-05-09 14:51:58 -0300

    backend test

--- api/db/db.js
+++ api/db/db.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 // const Brand = require('./models/Brand');
 // const Vehicle = require('./models/Vehicle');
 
-const db = new Sequelize('appTaller', null, null, {
-  host: 'localhost',
+const db = new Sequelize('app_taller', null, null, {
+  host: 'postgres://gmzleo90:tvIlMIp0o1QPepHFr6FqK7Z3hyhVTVuA@dpg-chbds93hp8u01612t510-a/app_taller',
   dialect: 'postgres',
   logging: false
 });
